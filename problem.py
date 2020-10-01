5ab6e802fb360361cca727f16524ec3bc8629dfb
Marissa Aguilera <marissaguilera@yahoo.com>
2020-10-01 10:07:10 -0700

    Final commit

--- problem.py
+++ problem.py
@@ -1,13 +1,6 @@
 # 169. Majority Element
 # Easy
 
-# 3760
-
-# 231
-
-# Add to List
-
-# Share
 # Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
 
 # You may assume that the array is non-empty and the majority element always exist in the array.
@@ -24,8 +17,26 @@
 #Leetcode Problem: Majority Element 
 
 
-def majority_element():
+def majority_element(nums):
     """Given an array find the majority element."""
+
+    nums.sort()
+
+    majority_num = nums[len(nums)//2]
+
+    return majority_num
 
 print(majority_element([3,2,3])) #3
 print(majority_element([2,2,1,1,1,2,2])) #2
+
+
+#ATTEMPTS
+    # majority_num = len(nums)//2
+    # print(majority_num)
+    # print(max(nums))
+
+    # for num in nums:
+    #     # print(max(num))
+    #     # if num.count() > 1:
+    #     if num/2 :
+    #         print(num)
